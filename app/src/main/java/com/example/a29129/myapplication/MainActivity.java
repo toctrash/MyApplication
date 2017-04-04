a8479d310e5daf7caa611984a892ffc7e1ddb5c6
toctrash <1124850791@qq.com>
2017-04-04 08:31:42 +0800

    add debug for info level

--- app/src/main/java/com/example/a29129/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a29129/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a29129.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.a29129.myapplication.utilities.MyTextUtil;
@@ -16,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
         MyTextUtil util = new MyTextUtil();
         TextView tv = (TextView) findViewById(R.id.activity_text_view);
         tv.setText(util.getTextValue());
+
+        for (int counter = 0; counter < 10; counter++) {
+            Log.i("MainActivity", "onCreate value: "+counter);
+        }
     }
 }
